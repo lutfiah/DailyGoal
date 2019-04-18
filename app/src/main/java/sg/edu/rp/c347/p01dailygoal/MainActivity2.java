7300e271d5d06c3b9b254e46b7b0c91ef313d65a
lutfiah <16023022@myrp.edu.sg>
2019-04-18 15:38:05 +0800

    Intermediate enhancements done

--- app/src/main/java/sg/edu/rp/c347/p01dailygoal/MainActivity2.java
+++ app/src/main/java/sg/edu/rp/c347/p01dailygoal/MainActivity2.java
@@ -3,6 +3,8 @@ package sg.edu.rp.c347.p01dailygoal;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
@@ -25,5 +27,13 @@ public class MainActivity2 extends AppCompatActivity {
         tv6.setText(info[1]);
         tv7.setText(info[2]);
         tv8.setText(info[3]);
+
+        Button btnClose = findViewById(R.id.button2);
+        btnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
